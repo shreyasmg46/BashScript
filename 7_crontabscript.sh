a0098bcc114c41a4a6f0532be5b52c23cefb5367
Shreyas <115518844+shreyasmg46@users.noreply.github.com>
2023-08-26 19:12:40 +0530

    Update 7_crontabscript.sh

--- 7_crontabscript.sh
+++ 7_crontabscript.sh
@@ -1,8 +1,16 @@
-#!/bin/bash
+crontab -e //It will open the vim editor 
 
+30 20 * * 1-5 
+// 30 refers to minutes in format MM
+// 20 refers to hour in format HH
+// * refers to date of month (* --> daily)
+// * refers to month in a year (* --> every month)
+// 1-5 refers to day of the week (1-5 --> Monday to Friday)
+
+#!/bin/bash
 echo "##############################################################"
 echo "Script for mysql backup"
-mysql -u root -p dummy ams > /home/ec2-user/bash_script/ams.sql
+30 20 * * 1-5 mysqldump -u root -p dummy ams &>> /home/ec2-user/bash_script/ams.sql
 date
 echo "###############################################################"
 echo "Database Backup Completed Successfully"
